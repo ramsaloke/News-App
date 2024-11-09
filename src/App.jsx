c28769aabcdd3673c1f2eb501786e64e3190cdf3
ram saloke <ramsaloke117@gmail.com>
2024-11-09 09:58:25 +0530

    changed the url image if it is null

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
           articles.map((article, index) => (
             <div key={index} className="blog-card">
               <img
-                src={article.urlToImage || "https://placehold.co/600x400"}
+                src={article.urlToImage || "https://plus.unsplash.com/premium_photo-1691223714882-57a432c4edaf?q=80&w=1781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                 alt={article.title}
               />
               <h2>
